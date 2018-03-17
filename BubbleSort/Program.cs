441ba9ec3a0298ca2486544ad182f2a72ba57231
Intezium <Intezium04@gmail.com>
2018-03-17 16:27:00 +0300

    Добавлена сортировка пузырьком

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace BubbleSort
 {
     class Program
     {
+        static void BubbleSort<T>(IList<T> list, IComparer<T> comparer)
+        {
+            for (int i = 0; i < list.Count; i++)
+                for (int j = list.Count - 1; j > i; j--)
+                    if (comparer.Compare(list[j], list[j - 1]) == -1)
+                    {
+                        T temp = list[j];
+                        list[j] = list[j - 1];
+                        list[j - 1] = temp;
+                    }
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
